24891d4ce5aa28d4608e6c395197d8e47eebdacd
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-08-22 22:35:35 +0100

    Use driver settings provided by MR

--- features/support/env.rb
+++ features/support/env.rb
@@ -18,18 +18,7 @@ After do |scenario|
 end
 
 AfterConfiguration do |config|
-  AppAutomateDriver.new(
-    bs_username,
-    bs_access_key,
-    bs_local_id,
-    bs_device,
-    app_location,
-    :accessibility_id,
-    {
-      'browserstack.deviceLogs' => true,
-      'browserstack.appium_version' => '1.15.0' # Temporary fix to allow running on iOS 10
-    }
-  )
+  AppAutomateDriver.new(bs_username, bs_access_key, bs_local_id, bs_device, app_location)
   $driver.start_driver
 end
 
